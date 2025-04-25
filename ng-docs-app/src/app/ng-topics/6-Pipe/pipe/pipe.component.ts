08825a5fc1c062ff6bbda3988ead5274d489afcc
Tayyip Kose <tayyipkose037@gmail.com>
2025-04-25 18:18:18 +0300

    pipes2

--- ng-docs-app/src/app/ng-topics/6-Pipe/pipe/pipe.component.ts
+++ ng-docs-app/src/app/ng-topics/6-Pipe/pipe/pipe.component.ts
@@ -12,4 +12,28 @@ export class PipeComponent {
     { name: 'Zeynep Demir', birthDate: '1998-11-22' },
     { name: 'Tuğba Aslan', birthDate: '1996-03-07' }
   ];
+
+
+  /* Angular Default Pipelar
+  1. **date:** Tarih formatlamak için kullanılır.
+     Kullanım: {{ value | date: 'short' }}
+
+  2. **uppercase:** Verilen metni büyük harfe dönüştürür.
+     Kullanım: {{ value | uppercase }}
+
+  3. **lowercase:** Verilen metni küçük harfe dönüştürür.
+     Kullanım: {{ value | lowercase }}
+
+  4. **currency:** Sayıyı para birimi formatında gösterir.
+     Kullanım: {{ value | currency: 'USD': true }}
+
+  5. **percent:** Sayıyı yüzde formatında gösterir.
+     Kullanım: {{ value | percent }}
+
+  6. **json:** JSON verisini biçimlendirerek gösterir.
+     Kullanım: {{ value | json }}
+
+  7. **slice:** Verilen dizi veya string'i dilimlemeye yarar.
+     Kullanım: {{ value | slice: 1:3 }}  // 1. index'ten 3. index'e kadar dilimler
+*/
 }
